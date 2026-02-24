5df62928f34f0cdd1fce29f19f6aad90c580056c
VoidOfMaya <mayaofthevoid@gmail.com>
2026-02-24 09:16:57 +0200

    feat(passport strategy)sets up jwt passport strategy and intializes it in app

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ import express from 'express'
 import 'dotenv/config'
 import passport from 'passport';
 import { authRouter } from './auth/authRouter.js';
+import { setupPassport } from './auth/authMiddleware.js';
 
 const app = express();
 //parse req string to json
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-
+setupPassport();
 app.use(passport.initialize());
 
 app.get('/',(req, res)=>{
--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,14 +1,29 @@
 
 import passport from 'passport';
 import {ExtractJwt, Strategy as JWTStrategy} from 'passport-jwt';
-import { ExtractJwt as extractJWT } from 'passport-jwt';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
+import {prisma} from '../lib/prisma.js'
+
 import 'dotenv/config';
 
 import {prisma} from '../lib/prisma.js';
 
 const isAuthenticated =     passport.authenticate('jwt',{session: false});
+function setupPassport(){
+    const options ={
+        jwtFormRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretKey: process.env.JWT_KEY,      
+    }
+    passport.use(new JWTStrategy(options,async (payload, done)=>{
+        try{
+        const user = await prisma.user.findUnique({where: {id: payload.id}})
+        if(!user) return done(null, false);
+        return done(null, user);
+        }catch(err){
+            done(err)
+        }
+
+    }))
+}
 
 passport.serializeUser((user,done)=>{
     done(null, user.id);
@@ -26,6 +41,7 @@ passport.deserializeUser(async(id, done)=>{
 function checkAuth (req, res, next){
 }
 export{
+    setupPassport,
     isAuthenticated,
     checkAuth,
 }
