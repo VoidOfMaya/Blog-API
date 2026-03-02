19807a47eca5ca9b10bc4d1cad114ca995c818dc
VoidOfMaya <mayaofthevoid@gmail.com>
2026-03-02 10:08:47 +0200

    refactor: splits validation arrays in indexvalidation

--- features/index/indexRouter.js
+++ features/index/indexRouter.js
@@ -5,13 +5,13 @@ import { getPosts, getPostById } from "./indexController.js";
 import { Router } from "express";
 import { midware } from "../middleWareOrchestrator.js";
 import { commentRouter } from "../comment/commentRouter.js";
-import { validateId } from '../../validations/indexValidation.js'
+import { validateId, commentValidation } from '../../validations/indexValidation.js'
 
 const indexRouter = Router({mergeParams: true});
 
 indexRouter.get('/', getPosts);
 indexRouter.get('/:id',validateId,getPostById);
-indexRouter.use('/:postId/comment',midware.isAuthenticated, validateId, commentRouter);
+indexRouter.use('/:postId/comment',midware.isAuthenticated, commentValidation, commentRouter);
 
 export{
     indexRouter
--- validations/indexValidation.js
+++ validations/indexValidation.js
@@ -1,9 +1,14 @@
 import { param } from "express-validator";
 
-const validateId = [param('id').trim().notEmpty().withMessage('id parameter is required')
-    .isNumeric().withMessage('id must be a number'),
-param('postId').trim().notEmpty().withMessage('id parameter is required')
-    .isNumeric().withMessage('id must be a number')]
+const validateId = [
+    param('id').trim().notEmpty().withMessage('id parameter is required')
+    .isInt().withMessage('id must be a number'),
+]
+const commentValidation =[
+    param('postId').trim().notEmpty().withMessage('id parameter is required')
+    .isInt().withMessage('id must be a number'),
+]
 export{
-    validateId
+    validateId,
+    commentValidation
 }
